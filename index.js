fbf2a468e831206356e7ce543f2c44d1597b426e
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-02-13 12:35:49 +0100

    app.listen kikommentálva

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 // Controller
-const controller = {
-  homePage: (req, res) => {
+function homePage(req, res) {
     res.render('index');
-  },
 };
 
 // Routes
-app.get('/', controller.homePage);
+app.get('/', homePage);
 
 // Szerver indítása
-app.listen(port, () => {
-  console.log(`Az alkalmazás fut a http://localhost:${port} címen.`);
-});
+//app.listen(port, () => {
+ // console.log(`Az alkalmazás fut a http://localhost:${port} címen.`);
+//});
